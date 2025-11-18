490117071b9fa0a2f526b52c00ca783445c10b87
Hafsa Abid <hafsaabd.82@gmail.com>
2025-11-18 17:03:58 +0500

    Update Summarizer.py

--- Summarizer.py
+++ Summarizer.py
@@ -9,7 +9,10 @@ import requests
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://summarizer-eight-pearl.vercel.app/"],
+    allow_origins=[
+        "https://summarizer-eight-pearl.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
