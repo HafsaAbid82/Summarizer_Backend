39f1e6d3a9e8fe93fd14bd3352ee98e6e119f27b
Hafsa Abid <hafsaabd.82@gmail.com>
2025-11-20 09:13:46 +0500

    Update Summarizer.py

--- Summarizer.py
+++ Summarizer.py
@@ -79,7 +79,7 @@ def post_data(request_data: SummaryRequest, request: Request):
     try:
         summarization_result = HF_client.summarization(
             article_text, 
-            model="facebook/bart-large-cnn"
+            model="Falconsai/text_summarization"
         )
         hf_summary = summarization_result[0]['summary_text'] 
     except Exception as e:
